447330af684719fff02daa8c237310600ae96b61
OnelMartinez <onelmartinezv@gmail.com>
2026-07-13 16:03:09 -0400

    feat: subir a github Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://webyonel.github.io',
+	base: '/tuyo-mio',
+});
